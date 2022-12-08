b34c0ea0744cb92d2bcd37069c3d993e2077da1d
KIEWITPLAZA\Sulav.Shakya <sulav.shakya@kiewit.com>
2022-12-08 10:31:16 -0600

    Configure react query

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import ErrorBoundary from './reusuable/ErrorBoundary';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <ErrorBoundary>
         <Toaster />
         <App />
       </ErrorBoundary>
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
